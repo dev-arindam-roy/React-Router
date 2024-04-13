b0e29ca82f4fede6d207a6b8501e6af75fa30a3f
Ari-Developer <arindam.roy.developer@gmail.com>
2024-04-13 19:09:50 +0530

    code evolution

--- react-router-bs5cdn/src/pages/HomePage.jsx
+++ react-router-bs5cdn/src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import HomeJumbotron from '../components/HomeJumbotron'
+import { ArindamAutoPassword } from 'arindam-auto-password'
 
 const HomePage = () => {
     return (
         <>
             <div className="container-fluid">
                 <HomeJumbotron />
-
+                <center>
+                    <h3>{ ArindamAutoPassword.strongPassword(16) }</h3>
+                </center>
             </div>
         </>
     )
